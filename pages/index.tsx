de244e66f298866e6fea7ebab7b5547980fe4b83
delorey <delorey@adobe.com>
2023-03-19 21:53:54 -0500

    cocktail creator

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,45 @@ import { useRef, useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import DropDown, { VibeType } from "../components/DropDown";
 import Footer from "../components/Footer";
-import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
+  const [ingredients, setIngredients] = useState("");
   const [vibe, setVibe] = useState<VibeType>("Professional");
-  const [generatedBios, setGeneratedBios] = useState<String>("");
+  const [generatedCocktail, setGeneratedCocktails] = useState<string>("");
 
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const ingredientsRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: "smooth" });
+  const scrollToCocktails = () => {
+    if (ingredientsRef.current !== null) {
+      ingredientsRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
-  const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
-    vibe === "Funny"
-      ? "Make sure there is a joke in there and it's a little ridiculous."
-      : null
-  }
-      Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
-    bio.slice(-1) === "." ? "" : "."
-  }`;
+  const prompt = `You are a bartender AI tasked with inventing new cocktails. You'll be given a list of potential ingredients and your job is to create new cocktail recipes with some, but not necessarily all, of the potential ingredients. You are encouraged to also include common household ingredients like spices, fruits, vegetables, and mixers like juices, milk, or soda water in the recipe. Your recipes should include no more than 3 types of liquor. The main priority is to create recipes that would be appealing to couples or friend groups for "cocktail nights at home." 
 
-  const generateBio = async (e: any) => {
+  The following is an example of prompt and response. Notice that not all available ingredients must be used.
+  Available ingredients: 
+  Coconut rum, tequila, triple sec, Tito's vodka
+  Recipe: 
+  Frozen margaritas
+  Ingredients: 
+  - 4 cups crushed ice
+  - 1 (6 ounce) can frozen limeade concentrate
+  - 6 fluid ounces tequila
+  - 2 fluid ounces triple sec
+  Directions: 
+  - Fill the blender with crushed ice. Pour in limeade concentrate, tequila, and triple sec. Blend until smooth. Pour into glasses and serve
+  
+  Available ingredients:
+  ${ingredients}
+  Recipe:`;
+
+  const generateCocktail = async (e: any) => {
     e.preventDefault();
-    setGeneratedBios("");
+    setGeneratedCocktails("");
     setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -64,34 +73,27 @@ const Home: NextPage = () => {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
+      setGeneratedCocktails((prev) => prev + chunkValue);
     }
-    scrollToBios();
+    scrollToCocktails();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Bio Generator</title>
+        <title>Cocktail Companion AI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p>Star on GitHub</p>
-        </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+          Host your next cocktail night with AI
         </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
+        <p className="text-slate-500 mt-5">
+          47,118 cocktails generated so far.
+        </p>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -102,36 +104,36 @@ const Home: NextPage = () => {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{" "}
+              Write a list of ingredients you have on hand{" "}
               <span className="text-slate-500">
-                (or write a few sentences about yourself)
+                (or just add a few of your favorite ingredients)
               </span>
               .
             </p>
           </div>
           <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            rows={4}
+            value={ingredients}
+            onChange={(e) => setIngredients(e.target.value)}
+            rows={6}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+              "e.g. Tito's vodka, coconut rum, blackberry brandy, triple sec, limes, peanut butter, soda water, etc."
             }
           />
-          <div className="flex mb-5 items-center space-x-3">
+          {/* <div className="flex mb-5 items-center space-x-3">
             <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
             <p className="text-left font-medium">Select your vibe.</p>
           </div>
           <div className="block">
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+          </div> */}
 
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              onClick={(e) => generateCocktail(e)}
             >
-              Generate your bio &rarr;
+              Generate your cocktail &rarr;
             </button>
           )}
           {loading && (
@@ -150,36 +152,39 @@ const Home: NextPage = () => {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <div className="space-y-10 my-10">
-          {generatedBios && (
+          {generatedCocktail && (
             <>
               <div>
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
+                  ref={ingredientsRef}
                 >
-                  Your generated bios
+                  Your generated cocktail
                 </h2>
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("2.")
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(generatedCocktail);
+                    toast("Bio copied to clipboard", {
+                      icon: "✂️",
+                    });
+                  }}
+                  key={generatedCocktail}
+                >
+                  {generatedCocktail.split("\n").map((item, key) => {
+                    return key === 0 ? (
+                      <h3 className="text-left font-bold" key={key}>
+                        {item}
+                      </h3>
+                    ) : (
+                      <p className="text-left" key={key}>
+                        {item}
+                      </p>
                     );
                   })}
+                </div>
               </div>
             </>
           )}
